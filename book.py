0486c9f4c94922cdcfca6264d52e150a7d721f79
Sultan <sultan.m.rashed@gmail.com>
2021-12-01 17:39:33 +0000

    Added template book
    Added a lot of templates and a class to handle the templates.

---
+++ book.py
@@ -0,0 +1,193 @@
+from typing import List, Tuple
+
+from templates import templates
+
+import numpy as np
+
+# x is up and down and y is left and right.
+class OpeningBook():
+    
+    opening_moves = {}
+    swap_book = {}
+    
+    def __init__(self, board_size, board) -> None:
+        self.board_size = board_size
+        self.board = board
+
+class TemplateBook():
+    
+    # Experimenting about whether I should use a detailed dict like this or just saving responses.
+    found_ingame = {
+        "Bottleneck": {"Intrusions": (), "Responses": {}},
+        "Edge Template 31b": {"Intrusions": (), "Responses": {}},
+        "Edge Template 2": {"Intrusions": (), "Responses": {}},
+        "Connections": []   
+    }
+    
+    responses = {}
+    
+    def __init__(self, board: List[List[str]], colour: str) -> None:
+        self.board = np.array(board)
+        self.filled_board = np.array(board)
+        self.checked_tiles = set()
+        self.checked_tiles_conns = set()
+        self.templates = templates
+        
+        #Can be either "R" or "B"
+        self.colour = colour
+    
+    def update_board(self, board: List[List[str]]) -> None or Tuple[int, int]:
+        """
+        Updates board by checking which tiles are new and finding out if any patterns are found.
+
+        Args:
+            board (List[List[str]]): Game board.
+            
+        Returns:
+            (Tuple[int, int] Optional): Can return move for player if immediate counter found.
+        
+        To-Do:
+        Idea for optimization: When our agent plays a move we should calculate more templates since our timer is not being used.
+        """
+        board = np.array(board)
+        x, y = int(np.where((self.board == board).all(axis=1) == False)[0]), int(np.where((self.board == board).all(axis=0) == False)[0])
+        if self.board[x,y] != self.colour and (x,y) in self.responses:
+            return self.responses[(x,y)]
+        self.check_for_all((x,y))
+        
+    def check_for_all(self, coords: Tuple[int, int]) -> List[List[int or str]]:
+        self.check_for_connection(coords)
+        self.check_for_bottleneck(coords)
+        self.check_for_edge_31b(coords)
+    
+    def check_for_connection(self, coords: Tuple[int, int]) -> None:
+        """
+        An example of a connection:
+              0 1 2 3 4
+             1    R
+              2  R* R*
+               3  R
+                4
+        Where R is the red player's tiles and R* is an empty tile that connects the red player's tiles together.
+        If the opponent attempts to block this move (called an intrustion), the red player should respond with an immediate response by taking
+        the other free tile.
+        
+        Coords is the most recently played move.
+        """
+        # Checking for red player first.
+        currConn = 0
+        x, y = coords
+        # Grabbing pattern from template dictionary.
+        while currConn <= len(self.templates["Connections"]):
+            pattern = self.templates["Connections"][currConn]
+            # Checking for necessary conditions to meet the requirement of a connection.
+            try:
+                if self.board[x + pattern[0][0], y + pattern[0][1]] == "0" and self.board[x + pattern[1][0], y + pattern[1][1]] == "0" \
+                    and self.board[x + pattern[2][0], y + pattern[2][1]] == self.board[x,y]:
+                        
+                        # We update our filled board and this tile to the list of checked tiles.
+                        self.filled_board[x + pattern[0][0], y + pattern[0][1]] = self.board[x,y] + "*"
+                        self.filled_board[x + pattern[1][0], y + pattern[1][1]] = self.board[x,y] + "*"
+                        self.checked_tiles_conns.add((x,y))
+                        break
+            except IndexError:
+                continue
+            currConn += 1
+    
+    def check_for_bottleneck(self, tile_coords: Tuple[int, int]) -> None:
+        """
+        Checks for the following pattern:
+              0  1 2 3 4 5
+             1     0 B 0 
+              2   0 0 0 0 
+               3   B R B
+                4
+                
+        Where R is red, B is blue, and 0 is an empty tile. Bottlenecks are vital in dictating the flow of the game defense wise.
+
+        Args:
+            tile_coords (Tuple[int, int]): Coordinates of tile checking.
+        """
+        x, y = tile_coords
+        
+        # Check colour of placed piece to find out opposite colour.
+        if self.board[x, y] == "R":
+            opposite = "B"
+        else:
+            opposite = "R"
+            
+        # Loop over all keys to check all possible bottlenecks.
+        for key in self.templates["Bottleneck"]:
+            
+            # If index out of range, continue since the pattern doesn't fit.
+            try:
+                # Runs conditional to check for validity of pattern. (Runs according to how bottlenecks are defined in dictionary above)
+                if self.check_validity(tile_coords, key, ["0", "0", self.board[x, y], self.board[x,y], opposite]):
+                    for x2, y2 in self.templates["Bottleneck"][key]["Responses"]:
+                        self.update_ingame_info("Bottleneck", tile_coords, (x2, y2), key)
+                    break        
+            except IndexError:
+                continue
+            
+    def check_for_edge_31b(self, tile_coords: Tuple[int, int]) -> None:
+        """
+        Checks for the following pattern:
+              0  1 2 3 4 5
+             1   0 0 0 0 0
+              2   0 0 0 0 
+               3   0 R 0
+                4
+                
+        Where R is the red player and 0 are empty spaces. This pattern has huge attacking potential.
+        
+        Args:
+            tile_coords (Tuple[int, int]): Coordinates of tile checking.
+        """
+        for key in self.templates["Edge Template 31b"]:
+            try:
+                if self.check_validity(tile_coords, key, ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0"]):
+                    for x2, y2 in self.templates["Edge Template 31b"][key]["Responses"]:
+                        self.update_ingame_info("Edge Template 31b", tile_coords, (x2, y2), key)
+                    break
+            except IndexError:
+                continue
+                
+    def check_validity(self, tile_coords: Tuple[int, int], to_check: Tuple[Tuple[int, int]], desired_result = List[str]) -> bool:
+        """
+        A helper function made for checking the validity of patterns by looping over the necessary requirements to be met for the
+        pattern to exist.
+
+        Args:
+            tile_coords (Tuple[int, int]): Coordinates of tile checking.
+            to_check (Tuple[Tuple[int, int]]): The tuple containing all the necessary coordinates for the pattern.
+            desired_result (List[str]): A list containing all necessary results from coordinates in to_check for pattern to be met.
+
+        Returns:
+            bool: True if pattern is met, False otherwise.
+        """
+        x, y = tile_coords
+        for i in range(len(to_check)):
+            if self.board[x + to_check[i][0], y + to_check[i][1]] == desired_result[i]:
+                continue
+            else:
+                return False
+        return True
+    
+    def update_ingame_info(self, template_name: str, tile_coords: Tuple[int, int], intrusion_coords: Tuple[int, int], key: Tuple[Tuple[int, int]]) -> None:
+        """
+        A helper function dedicated to updating our filled in board (includes virtual tiles) and updates in_game dictionary with responses to certain moves,
+        translated from relative positions to absolute positions on the board.
+
+        Args:
+            template_name (str): Name of template updating.
+            tile_coords (Tuple[int, int]): Coordinates of tile checking.
+            intrusion_coords (Tuple[int, int]): Coordinates where counters are required to play against.
+            key (Tuple[Tuple[int, int]]): Key from template dictionary that includes all necessary positional coordinates for pattern. (Used here to access dictionary).
+        """
+       
+        x, y = tile_coords
+        x2, y2 = intrusion_coords
+        if self.board[x,y] == self.colour:
+            self.found_ingame[template_name]["Responses"][(x+x2, y+y2)] = ( self.templates[template_name][key]["Responses"][(x2, y2)][0] + x, self.templates[template_name][key]["Responses"][(x2, y2)][1] + y)
+            self.responses[(x+x2, y+y2)] = self.found_ingame[template_name]["Responses"][(x+x2, y+y2)]
+        self.filled_board[x + x2, y + y2] = self.board[x,y] + "*"
---
+++ templates.py
@@ -0,0 +1,237 @@
+# I did letter first followed by number, based on this website http://www.lutanho.net/play/hex.html
+
+templates = {
+    
+    "Bottleneck": {
+        
+        """
+            When checking for bottleneck, we check for the following tuple:
+                ( (x, y), (x2, y2), (x3, y3), (x4, y4), (x5, y5) )
+            Where:
+                ( (Relative to player on edge position empty tile), (Relative to player on edge position empty tile), (Players own other tile relative to the first),
+                    (Player's own other tile relative to the first), (Enemy's tile relative to the first) )
+            We return tiles to mark as owned intrusion tiles.
+        """
+        
+            # Blue defending
+            ( (0, -1), (1, -1), (0, -2), (2, -2), (1, -2) ): {
+                
+                "Intrusions": ( (0, -1), (1, -1), (-1, 0), (1, 0), (2, -1), (-1, -1) ),
+                "Responses": {
+                    # Intrusion Tile    # Response
+                    (0, -1):            (-1, 0),
+                    (1, -1):            (1, 0),
+                    (-1, 0):            (0, -1),
+                    (1, 0):             (1, -1),
+                    (-1, -1):           (0, -1)
+                }
+            },
+            
+            ( (0, 1), (-1, 1), (-2, 2), (0, 2), (-1, 2) ): {
+            
+                "Intrusions": ( (0, 1), (-1, 1), (1, 0), (-1, 0), (1, 1), (-2, 1) ),
+                "Responses": {
+                    # Intrusion Tile    # Response
+                    (0, 1):             (1, 0),
+                    (-1, 1):            (-1, 0),
+                    (1, 0):             (0, 1),
+                    (-1, 0):            (-1, 1),
+                    (1, 1):             (0, 1),
+                    (-2, 1):            (-1, 1)
+                }
+            },
+        
+            # Red defending
+            ( (-1, 1), (-1, 0), (-2, 0), (-2, 2), (-1, 1) ):{
+                
+                "Intrusions": ( (-1, 1), (-1, 0), (0, 1), (0, -1), (-1, -1), (-1, 2) ),
+                "Responses": {
+                    # Intrusion Tile    # Response
+                    (-1, 1):            (0, 1),
+                    (-1, 0):            (0, -1),
+                    (0, 1):             (-1, 1),
+                    (0, -1):            (0, 1),
+                    (-1, -1):           (-1, 0),
+                    (-1, 2):            (-1, 1)
+                }
+            },
+            
+            ( (1, 0), (1, -1), (2, 0), (2, -2), (2, -1) ): {
+                "Intrusions": ( (0, 1), (1, 1), (1, 0), (1, -1), (1, -2), (0, -1) ),
+                "Responses": {
+                        # Intrusion Tile    # Response
+                        (0, 1):             (1, 0),
+                        (1, 1):             (1, 0),
+                        (1, 0):             (0, 1),
+                        (1, -1):            (0, -1),
+                        (1, -2):            (1, -1), 
+                        (0, -1):            (1, -1)
+                }
+            }
+        },
+    
+    # Remember to check that edge templates are only checked if tile is on edge.
+    # Remember to check that player only plays his own counters. I should only add the player's counters to the counter list.
+    "Edge Template 31b": {
+        ( (0, -1), (0, 1), (-1, -1), (-1, 0), (-1, 1), (-1, 2), (-2, -1), (-2, 0), (-2, 2), (-2, 3) ): {
+            "Intrusions": ( (0, -1), (0, 1), (-1, -1), (-1, 0), (-1, 1), (-1, 2), (-2, -1), (-2, 0), (-2, 2), (-2, 3) ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                (0, -1):            (-1, 2),
+                (0, 1):             (-1, -1),
+                (-1, -1):           (-1, 2),
+                (-1, 0):            (-1, 2),
+                (-1, 1):            (-1, -1),
+                (-1, 2):            (-1, -1),
+                (-2, -1):           (-1, 2),
+                (-2, 0):            (-1, 2),
+                (-2, 2):            (-1, -1),
+                (-2, 3):            (-1, -1)
+            }
+        },
+        
+        ( (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1), (2, -1), (-1, -2), (0, -2), (2, -2), (3, -2) ): {
+            "Intrusions": ( (-1, 0), (1, 0), (-1, -1), (0, -1), (1, -1), (2, -1), (-1, -2), (0, -2), (2, -2), (3, -2) ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                (-1, 0):            (2, -1),
+                (1, 0):             (-1, -1),
+                (-1, -1):           (2, -1),
+                (0, -1):            (2, -1), 
+                (1, -1):            (-1, -1),
+                (2, -1):            (-1, -1),
+                (-1, -2):           (2, -1),
+                (0, -2):            (2, -1),
+                (2, -2):            (-1, -1),
+                (3, -2):            (-1, -1)
+            }
+        },
+        
+        ( (-1, 0), (1, 0), (-2, 1), (-1, 1), (0, 1), (1, 1), (-3, 2), (-2, 2), (0, 2), (1, 2) ): {
+            "Intrusions": ( (-1, 0), (1, 0), (-2, 1), (-1, 1), (0, 1), (1, 1), (-3, 2), (-2, 2), (0, 2), (1, 2) ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                (-1, 0):            (1, 1),
+                (1, 0):             (-2, 1), 
+                (-2, 1):            (1, 1),
+                (-1, 1):            (1, 1),
+                (0, 1):             (-2, 1),
+                (1, 1):             (-2, 1),
+                (-3, 2):            (1, 1),
+                (-2, 2):            (1, 1),
+                (0, 2):             (-2, 1),
+                (1, 2):             (-2, 1)
+            }
+        },
+        
+        ( (0, -1), (0, 1), (1, -2), (1, -1), (1, 0), (1, 1), (2, -3), (2, -2), (2, 0), (2, 1) ): {
+            "Intrusions": ( (0, -1), (0, 1), (1, -2), (1, -1), (1, 0), (1, 1), (2, -3), (2, -2), (2, 0), (2, 1) ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                (0, -1):            (1, 1),
+                (0, 1):             (1, -2),
+                (1, -2):            (1, 1), 
+                (1, -1):            (1, 1),
+                (1, 0):             (1, -2),
+                (1, 1):             (1, -2),
+                (2, -3):            (1, 1),
+                (2, -2):            (1, 1),
+                (2, 0):             (1, -2),
+                (2, 1):             (1, -2)
+            }
+        },
+        
+    },
+    
+    "Edge Template 2": {
+        (  ): {
+            "Intrusions": ( (-1, 0), (-1, 1) ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                (-1, 0):            (-1, 1),
+                (-1, 1):            (-1, 0)
+            }
+        },
+        
+        (  ): {
+            "Intrusions": ( (0, -1), (1, -1) ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                (0, 1):             (1, -1),
+                (1, -1):            (0, 1),
+            }
+        }, 
+        
+        (  ): {
+            "Intrusions": ( (-1, 1), (0, 1) ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                (-1, 1):            (0, 1),
+                (0, 1):             (-1, 1)
+            }
+        },
+        
+        (  ): {
+            "Intrusions": ( (1, -1), (1, 0) ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                (1, -1):            (1, 0),
+                (1, 0):             (1, -1)
+            }
+        }
+    },
+    # Update like the rest in terms of formatting.
+    "Connections": [
+        
+        """
+            When checking for connections, we check for the following tuple:
+                ( (x, y), (x2, y2), (x3, y3) )
+            Where:
+                ( (Relative to player position's empty tile), (Relative to player position's empty tile), (Players own other tiles) )
+        """
+        
+        ( (-1, 0), (0, -1), (-1, -1) ),
+        ( (1, -1), (0, -1), (1, -2) ),
+        ( (1, -1), (1, 0), (2, -1) ),
+        ( (1, 0), (0, 1), (1, 1) ),
+        ( (-1, 1), (0, 1), (-1, 2) ),
+        ( (-1, 1), (-1, 0), (-2, 1) )
+        
+    ],
+    
+    "Ladder 3rd Row": {
+        # Red player
+        ( (0, -1), (1, -1), (2, -3) ): {
+            "Intrusions": ( (0, -1), (1, -1), (2, -3), (1, -1) ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                
+            }
+        },
+        
+        (  ): {
+            "Intrusions": (  ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                
+            }
+        },
+        
+        (  ): {
+            "Intrusions": (  ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                
+            }
+        },
+        
+        (  ): {
+            "Intrusions": (  ),
+            "Responses": {
+                # Intrusion Tile    # Response
+                
+            }
+        }
+    }    
+
+}   
